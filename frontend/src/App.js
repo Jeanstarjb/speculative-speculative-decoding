95df3dc7e34ff678eaff3950a699a6dabd732197
Jeff Munyaka <161038752+Jeanstarjb@users.noreply.github.com>
2026-07-12 00:54:18 +0300

    feat(frontend): add basic React App component

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Speculative Speculative Decoding</h1>
+        <p>Welcome to the SSD frontend!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
